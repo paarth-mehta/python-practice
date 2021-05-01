2fb208762e63a433dc00d93609eb39c13f5c58e6
paarth-mehta <paarth.mehta.au@gmail.com>
2021-05-01 10:10:09 +1000

    150

---
+++ 150_questions/Question_4.py
@@ -0,0 +1,6 @@
+from math import pi
+from math import pow
+
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * pow(r, 2)))
---
+++ 150_questions/lname_fname.py
@@ -0,0 +1,9 @@
+# fanme = input("What is your first name?")
+# lname = input("What is your last name?")
+# print(lname, fanme)
+
+
+
+name = input("Enter complete name: ")
+print("Reverse name: ")
+print(" ".join(name.split()[::-1]))
